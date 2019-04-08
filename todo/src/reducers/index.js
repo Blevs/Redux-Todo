90289d8019b54c1049e41e7172a2238dbea2afa4
Henry Blevins <heblevi@gmail.com>
2019-04-08 17:14:41 -0400

    Implement reducers for add and toggle

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,9 +1,25 @@
+import { TODO_ADD, TODO_TOGGLE } from '../actions';
+
 const initialState = {
   todos: []
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+  case TODO_ADD: {
+    return {
+      ...state,
+      todos: state.todos.concat({
+        id: Date.now(),
+        task: action.task,
+        completed: false
+      })};
+  }
+  case TODO_TOGGLE: {
+    const todoIndex = state.todos.findIndex(({id}) => id === action.id);
+    todoIndex >= 0 && (state.todos[todoIndex].completed = !state.todos[todoIndex].completed);
+    return state;
+  }
   default:
     return state;
   }
