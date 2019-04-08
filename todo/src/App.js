04aeae5fa96ff11a53d6471d7bff252daf48bcbe
Henry Blevins <heblevi@gmail.com>
2019-04-08 17:02:12 -0400

    Connect App to redux store

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        TODO
-      </div>
-    );
-  }
-}
 
-export default App;
+const App = ({todos}) => {
+  return (
+    <div className="App">
+      TODO
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect(mapStateToProps, {})(App);
