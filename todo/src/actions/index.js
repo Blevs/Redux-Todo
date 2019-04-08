5e535cb56491dd9779a0de8c77b7b7a1ec58ec0f
Henry Blevins <heblevi@gmail.com>
2019-04-08 17:07:38 -0400

    Add add and toggle actions

---
+++ todo/src/actions/index.js
@@ -0,0 +1,16 @@
+export const TODO_ADD = 'TODO_ADD';
+export const TODO_TOGGLE = 'TODO_TOGGLE';
+
+export const todoAdd = (task) => {
+  return {
+    type: TODO_ADD,
+    task: task
+  };
+};
+
+export const todoToggle = (id) => {
+  return {
+    type: TODO_ADD,
+    id: id
+  };
+};
